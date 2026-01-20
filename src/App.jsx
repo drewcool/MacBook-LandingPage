e6da1cc302fe6dc1ee3b2178a43a326d3bdd94cc
Ashish Kumar <155881265+drewcool@users.noreply.github.com>
2026-01-20 16:08:14 +0530

    added smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import ProductViewer from './components/ProductViewer'
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
-import Showcase from './components/Showcase';
-import Performance from './components/Performance';
-import Features from './components/Features';
-import Highlights from './components/Highlights';
-import Footer from './components/Footer';
+import { useEffect } from "react";
+import Lenis from "lenis";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove(lenis.raf);
+    };
+  }, []);
+
   return (
     <main>
       <NavBar />
@@ -24,7 +42,7 @@ const App = () => {
       <Highlights />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
